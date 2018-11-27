c502b40df8ebabb6caec9f11f82302e9e36dfcfb
Hiswe <hiswehalya@gmail.com>
2018-11-27 11:47:02 +0700

    DEPENDENCIES – update
    fix event-stream

--- gulpfile.js
+++ gulpfile.js
@@ -3,49 +3,61 @@
 // use a regular es5 gulpfile
 //  -> so we can really test what is using a Babel compiled module
 
-var del           = require('del');
-var gulp          = require('gulp');
-var gutil         = require('gulp-util');
-var babel         = require('gulp-babel');
-var jsbeautifier  = require('gulp-jsbeautifier');
-var merge         = require('merge-stream');
-var concat        = require('gulp-concat');
-var rc            = require('rc')('gds');
-var argv          = require('yargs').argv;
+var del = require('del');
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var jsbeautifier = require('gulp-jsbeautifier');
+var merge = require('merge-stream');
+var rc = require('rc')('gds');
+var argv = require('yargs').argv;
 
-gulp.task('clean-build', function (cb) {
+function cleanBuild(cb) {
   return del(['dist'], cb);
-});
+}
 
-gulp.task('build', ['clean-build'], function () {
-  var jsBabel = gulp
-    .src('src/*.js')
-    .pipe(babel({
-      presets:  ['es2015'],
-    }));
-  var listFiles = gulp
-    .src('src/*.list');
-  return merge(jsBabel, listFiles)
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('clean-build', cleanBuild);
+
+function buildJs() {
+  var jsBabel = gulp.src('src/*.js').pipe(
+    babel({
+      presets: ['@babel/env'],
+    })
+  );
+  var listFiles = gulp.src('src/*.list');
+  return merge(jsBabel, listFiles).pipe(gulp.dest('dist'));
+}
+
+const build = gulp.series(cleanBuild, buildJs);
+
+gulp.task('build', build);
 
 // test dump task
-gulp.task('clean-dump', function (cb) {
+function cleanDump(cb) {
   return del(['tmp'], cb);
-});
+}
 
-gulp.task('dump', ['clean-dump'], function () {
-  var dumpify       = require('./dist/index');
-  return dumpify({
-    domain:   rc.domain,
-    apikey:   rc.apikey,
-    password: rc.password,
-    debug:    argv.debug != null,
-  })
-  // better to have a nice indentation
-  .pipe(jsbeautifier({
-    indentSize: 2,
-    logSuccess: false
-  }))
-  .pipe(gulp.dest('tmp'));
-});
+gulp.task('clean-dump', cleanDump);
+
+function dumpify() {
+  var dumpify = require('./dist/index');
+  return (
+    dumpify({
+      domain: rc.domain,
+      apikey: rc.apikey,
+      password: rc.password,
+      debug: argv.debug != null,
+    })
+      // better to have a nice indentation
+      .pipe(
+        jsbeautifier({
+          indentSize: 2,
+          logSuccess: false,
+        })
+      )
+      .pipe(gulp.dest('tmp'))
+  );
+}
+
+const dump = gulp.series(cleanDump, dumpify);
+
+gulp.task('dump', dump);
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,16 @@
 import request from 'request';
-import gutil from 'gulp-util';
+import Vinyl from 'vinyl';
+import c from 'ansi-colors';
+import fancyLog from 'fancy-log';
+import PluginError from 'plugin-error';
 
-const name    = 'gulp-dump-shopify';
+const name = 'gulp-dump-shopify';
 
 function error(content) {
-  return new gutil.PluginError(name, content);
+  return new PluginError(name, content);
 }
 
-const config  = {
+const config = {
   domain: false,
   apikey: false,
   password: false,
@@ -15,12 +18,12 @@ const config  = {
 };
 
 // logger
-const prefix      = gutil.colors.cyan(name);
+const prefix = c.cyan(name);
 function logger(options) {
-  if (!options.debug) return function () {}
+  if (!options.debug) return function() {};
   return function log(...args) {
-    gutil.log(prefix, ...args);
-  }
+    fancyLog(prefix, ...args);
+  };
 }
 
 // send an error if 1 of the crucial options are missing
@@ -29,36 +32,38 @@ function checkOptions(options, context) {
     context.emit('error', error('domain, apikey && password are mandatory'));
     return false;
   }
-  return true
+  return true;
 }
 
 // make an authentified request
 function rq(options) {
-  const domain      = options.domain;
-  const auth        = {
-    'auth': {
-      'user': options.apikey,
-      'pass': options.password,
-    }
+  const domain = options.domain;
+  const auth = {
+    auth: {
+      user: options.apikey,
+      pass: options.password,
+    },
   };
 
   return function apiRequest(path, callback) {
     path = `${domain}/admin/${path}`;
     if (!/\.json$/.test(path)) path = `${path}.json`;
-    request( path, auth, callback);
-  }
+    request(path, auth, callback);
+  };
 }
 
 // create a new vinyl file and push it right away
-function createFile(fileName, content, cwd = './' ) {
-  fileName  = /\.json$/.test(fileName) ? fileName : fileName + '.json';
-  content   = typeof content === 'string' ? content : JSON.stringify(content);
-  this.push(new gutil.File({
-    cwd:  cwd,
-    base: './',
-    path: cwd === './' ? fileName : cwd + fileName,
-    contents: new Buffer(content)
-  }));
+function createFile(fileName, content, cwd = './') {
+  fileName = /\.json$/.test(fileName) ? fileName : fileName + '.json';
+  content = typeof content === 'string' ? content : JSON.stringify(content);
+  this.push(
+    new Vinyl({
+      cwd: cwd,
+      base: './',
+      path: cwd === './' ? fileName : cwd + fileName,
+      contents: new Buffer.from(content),
+    })
+  );
 }
 
-export {name, config, rq as request, checkOptions, error, logger, createFile};
+export { name, config, rq as request, checkOptions, error, logger, createFile };
